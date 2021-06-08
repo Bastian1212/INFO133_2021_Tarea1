66c12bb26f184da6cc0a625cac70a7a1c16a5cfc
Bastian1212 <B>
2021-06-07 23:15:53 -0400

    v3

--- info133Tarea.py
+++ info133Tarea.py
@@ -1,4 +1,7 @@
 from pymongo import MongoClient
+import os
+import time
+#--------------------#
 client = MongoClient("localhost")
 #--------------------#
 db = client["proyecto_Fusa"]
@@ -41,9 +44,9 @@ def agregar_Persona(personas,nombreP,apellidoP,rutP):
     })
 
 
-def Arregar_Segmento_audio(segmento, listaSegmentos, nombre_fuentes,descripcion,ID_analizador ):
+def Agregar_Segmento_audio(segmento, listaSegmentos, nombre_fuentes,descripcion,ID_analizador ):
 
-    segmento.inset_one({
+    segmento.insert_one({
         "ID" : listaSegmentos[0],
         "formato" : listaSegmentos[1], 
         "duracion" : listaSegmentos[2],
@@ -73,6 +76,10 @@ def analizar_automaticamente(seg, ID_seg, nombre_fuente):
     })
 
 
+def eliminarData():
+    client.drop_database("proyecto_Fusa")
+    
+
 
 
 
@@ -82,10 +89,52 @@ if __name__ == "__main__":
     archivos = db["Archivos_audios"]
     personas = db["Usarios"]
     segmentos = db["segmentos"]
+    listaNull = ["null","null","null","null","null" ,"null"]
+    bole = True
 
-    agregar_Persona(personas,"bastian","villanueva","20135031k")
-    client.drop_database("proyecto_Fusa")
-   
+    while True: 
+        print("1 - Ingresa un usuario ")
+        print("2 - Agregar Archivo de audio  ")
+        print("3 - Analizar Automaticamente ")
+        print("4 - Analizar Manualmente    ")
+        print("5 - eliminar la base de datos ")
+        print("6 - salir ")
+        ele_menu = int(input(" : "))
+
+        if ele_menu == 1:
+            nom  = input("Ingresa tu nombre : ")
+            apell = input("Ingresa tu apellido :") 
+            rut = input("Ingresa tu rut :  ") 
+            agregar_Persona(personas,nom,apell,rut)
+            print("el usuario ", nom.upper() , "fue agregado a la base de datos exitosamente ")
+            time.sleep(2)
+            os.system("clear")
+        if ele_menu == 2: 
+            os.system("clear")
+        if ele_menu == 3:
+            os.system("clear")
+        if ele_menu == 4 :
+            os.system("clear") 
+
+
+        if ele_menu == 5 : 
+            eliminarData()
+            print("base de datos eliminada")
+            time.sleep(2)
+            os.system("clear")
+           
+        
+        if ele_menu == 6 :
+            
+            os.system("clear")
+            print("Adios.....  :)") 
+            break
+        
+        os.system("clear")
+
+        
+
+        
 
 
 
